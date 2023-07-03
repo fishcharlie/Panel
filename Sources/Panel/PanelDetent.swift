7170525e58ff92982d29736062c000e46f2ec416
Charlie Fish <fishcharlie@me.com>
2023-07-03 15:05:25 -0600

    Improving PanelDetent

--- Sources/Panel/PanelDetent.swift
+++ Sources/Panel/PanelDetent.swift
@@ -7,37 +7,37 @@
 import SwiftUI
 
 public enum PanelDetent: CaseIterable {
-    case large
-    case medium
-    case small
+	case large
+	case medium
+	case small
 
-    func height(for environment: PanelEnvironment) -> CGFloat {
-        if environment.isCompact {
-            return compactHeight(for: environment)
-        } else {
-            return regularHeight(for: environment)
-        }
-    }
+	func height(for environment: PanelEnvironment) -> CGFloat {
+		if environment.isCompact {
+			return compactHeight(for: environment)
+		} else {
+			return regularHeight(for: environment)
+		}
+	}
 
-    private func regularHeight(for environment: PanelEnvironment) -> CGFloat {
-        switch self {
-        case .large:
-            return environment.geometry.size.height - (environment.insets.top + 24.0 + environment.insets.bottom)
-        case .medium:
-            return environment.geometry.size.height * 0.4
-        case .small:
-            return CGFloat(90)
-        }
-    }
+	private func regularHeight(for environment: PanelEnvironment) -> CGFloat {
+		switch self {
+		case .large:
+			return environment.geometry.size.height - (environment.insets.top + 24.0 + environment.insets.bottom)
+		case .medium:
+			return environment.geometry.size.height * 0.4
+		case .small:
+			return CGFloat(90)
+		}
+	}
 
-    private func compactHeight(for environment: PanelEnvironment) -> CGFloat {
-        switch self {
-        case .large:
-            return environment.geometry.size.height - (environment.insets.top + 12.0)
-        case .medium:
-            return environment.geometry.size.height * 0.4
-        case .small:
-            return CGFloat(90)
-        }
-    }
+	private func compactHeight(for environment: PanelEnvironment) -> CGFloat {
+		switch self {
+		case .large:
+			return (environment.geometry.size.height + environment.geometry.safeAreaInsets.bottom) - 10
+		case .medium:
+			return environment.geometry.size.height * 0.4
+		case .small:
+			return environment.geometry.safeAreaInsets.bottom + 50
+		}
+	}
 }
